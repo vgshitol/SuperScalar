128897f4ab94544a9dfebc7c7fc4c0a2f1ebfb66
Vishal Shitole <vgshitol@ncsu.edu>
2018-12-02 22:12:28 -0500

    Initial Code

---
+++ project3_read_trace/c_files/sim_proc.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sim_proc.h"
+
+/*  argc holds the number of command line arguments
+    argv[] holds the commands themselves
+
+    Example:-
+    sim 256 32 4 gcc_trace.txt
+    argc = 5
+    argv[0] = "sim"
+    argv[1] = "256"
+    argv[2] = "32"
+    ... and so on
+*/
+int main (int argc, char* argv[])
+{
+    FILE *FP;               // File handler
+    char *trace_file;       // Variable that holds trace file name;
+    proc_params params;       // look at sim_bp.h header file for the the definition of struct proc_params
+    int op_type, dest, src1, src2;  // Variables are read from trace file
+    unsigned long int pc; // Variable holds the pc read from input file
+    
+    if (argc != 5)
+    {
+        printf("Error: Wrong number of inputs:%d\n", argc-1);
+        exit(EXIT_FAILURE);
+    }
+    
+    params.rob_size     = strtoul(argv[1], NULL, 10);
+    params.iq_size      = strtoul(argv[2], NULL, 10);
+    params.width        = strtoul(argv[3], NULL, 10);
+    trace_file          = argv[4];
+    printf("rob_size:%lu "
+            "iq_size:%lu "
+            "width:%lu "
+            "tracefile:%s\n", params.rob_size, params.iq_size, params.width, trace_file);
+    // Open trace_file in read mode
+    FP = fopen(trace_file, "r");
+    if(FP == NULL)
+    {
+        // Throw error and exit if fopen() failed
+        printf("Error: Unable to open file %s\n", trace_file);
+        exit(EXIT_FAILURE);
+    }
+    
+    while(fscanf(FP, "%lx %d %d %d %d", &pc, &op_type, &dest, &src1, &src2) != EOF)
+    {
+        
+        printf("%lx %d %d %d %d\n", pc, op_type, dest, src1, src2); //Print to check if inputs have been read correctly
+        /*************************************
+            Add calls to OOO simulator here
+        **************************************/
+    }
+    return 0;
+}
